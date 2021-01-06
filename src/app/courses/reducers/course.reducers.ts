8b060459ceac4b5b4fa8e4c444139ec7e1407f43
CliffDaniel <cliff.clemente@vglsy.com>
2021-01-06 16:01:37 +0100

    lesson 33

--- src/app/courses/reducers/course.reducers.ts
+++ src/app/courses/reducers/course.reducers.ts
@@ -5,20 +5,25 @@ import { compareCourses, Course } from "../model/course";
 
 
 export interface CoursesState extends EntityState<Course>{
+    allCoursesLoaded: boolean
 }
 
 export const adapter = createEntityAdapter<Course>({
     sortComparer: compareCourses
 });
 
-export const initialCoursesState = adapter.getInitialState();
+export const initialCoursesState = adapter.getInitialState({
+    allCoursesLoaded: false
+});
 
 export const coursesReducer = createReducer (
 
     initialCoursesState,
 
     on(CourseActions.allCoursesLoaded,
-        (state, action) => adapter.addAll(action.courses, state))
+        (state, action) => adapter.addAll(
+            action.courses, 
+            {...state, allCoursesLoaded: true}))
 )
 
 export const {selectAll} = adapter.getSelectors();
